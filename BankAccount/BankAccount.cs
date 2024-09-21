620f7c76e4c7bd4257d9d9d9a3c0c45be5a001df
Edrisa Jallow <edrisa.j98@hotmail.com>
2024-09-21 12:44:15 +0200

    Uppdatering: Klar med hemuppgiften.

--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,10 +10,36 @@ namespace BankAccount
     public class BankAccount
     {
         //Lägg till Egenskaper (fields)
+        public string AccountNumber { get; set; }
+        public string AccountHolder { get; set; }
+        public double Balance { get; set; }
 
         //Lägg till Konstruktor
+        public BankAccount(string accountNumber, string accountHolder, double balance) 
+        {
+            AccountNumber = accountNumber;
+            AccountHolder = accountHolder;
+            Balance = balance; 
+        }
 
         //Lägg till Metoder
+        public void Deposit(double newDeposit)
+        {
+            Balance += newDeposit;
+            Console.WriteLine($"Deposited: {newDeposit}, New Balance: {Balance}\n");
+        }
+
+        public void Withdraw(double newWhitdraw)
+        {
+            Balance -= newWhitdraw;
+
+            Console.WriteLine($"Whitdraw: {newWhitdraw}, New Balance: {Balance}\n");
+        }
+
+        public void DisplayBalance() 
+        {
+            Console.WriteLine($"Balance: {Balance}");
+        }
 
         //Lycka till! :)
     }
